064cae30c284366f1082f928180d08c73dc0fc12
Pan Hezeng <panhezeng@gmail.com>
2019-02-14 19:02:32 +0800

    优化依赖模块版本

--- src/index.js
+++ src/index.js
@@ -1,52 +1,59 @@
-const shell = require('shelljs')
+const shell = require("shelljs");
 
 export class BeforeRunWebpackPlugin {
   /**
    * options {cb, sed }
    * @param options
    */
-  constructor (options) {
-    this.options = options
+  constructor(options) {
+    this.options = options;
   }
 
-  apply (compiler) {
-
-    const optionsIsObj = (Object.prototype.toString.call(this.options) === '[object Object]')
+  apply(compiler) {
+    const optionsIsObj =
+      Object.prototype.toString.call(this.options) === "[object Object]";
 
     let cb = () => {
-      let outputPath = 'docs'
-      let srcPath = 'index.html'
-      shell.rm('-rf', outputPath)
-      shell.mkdir(outputPath)
-      shell.cp('-R', srcPath, outputPath)
+      let outputPath = "docs";
+      let srcPath = "index.html";
+      shell.rm("-rf", outputPath);
+      shell.mkdir(outputPath);
+      shell.cp("-R", srcPath, outputPath);
       if (optionsIsObj && this.options.sed) {
         switch (this.options.sed) {
-          case 'react':
-            shell.sed('-i', /(react-dom|react)\.development\.js/g, '$1.production.min.js', outputPath + '/' + srcPath)
-            break
-          case 'vue':
-            shell.sed('-i', 'vue.js', 'vue.min.js', outputPath + '/' + srcPath)
-            break
+          case "react":
+            shell.sed(
+              "-i",
+              /(react-dom|react)\.development\.js/g,
+              "$1.production.min.js",
+              outputPath + "/" + srcPath
+            );
+            break;
+          case "vue":
+            shell.sed("-i", "vue.js", "vue.min.js", outputPath + "/" + srcPath);
+            break;
         }
       }
-    }
+    };
 
     if (optionsIsObj) {
-      if (Object.prototype.toString.call(this.options.cb) === '[object Function]') {
-        cb = this.options.cb
+      if (
+        Object.prototype.toString.call(this.options.cb) === "[object Function]"
+      ) {
+        cb = this.options.cb;
       }
     }
 
     const run = (compiler, callback) => {
-      cb()
-      callback()
-    }
+      cb();
+      callback();
+    };
 
     // webpack4以上使用hooks
     if (compiler.hooks) {
-      compiler.hooks.run.tapAsync('beforeRunWebpackPlugin', run)
+      compiler.hooks.run.tapAsync("beforeRunWebpackPlugin", run);
     } else {
-      compiler.plugin('run', run)
+      compiler.plugin("run", run);
     }
   }
 }
